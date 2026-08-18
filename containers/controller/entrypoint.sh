184fc8038624d017265d8831eee9d3305b2f7c3c
Nicolas Perozzi <perozzinicolas@gmail.com>
2026-08-18 21:19:56 +0200

    Controller container initialize known_hosts automaticaly.

--- containers/controller/entrypoint.sh
+++ containers/controller/entrypoint.sh
@@ -25,5 +25,11 @@ cp "$KEY_FILE.pub" /shared/authorized_key
 chown puppet:puppet /shared/authorized_key
 chmod 644 /shared/authorized_key
 
+# Initialize known_hosts automaticaly
+ssh-keyscan test_node_01 test_node_02 > /home/puppet/.ssh/known_hosts
+
+chown puppet:puppet /home/puppet/.ssh/known_hosts
+chmod 644 /home/puppet/.ssh/known_hosts
+
 # Start the next command provided by Docker
 exec "$@"
