2bef58784f7fc3a45cf30543d1083624749357a1
Nicolas Perozzi <perozzinicolas@gmail.com>
2026-08-19 10:25:15 +0200

    Add initial Puppet site manifest for workshop environment

---
+++ environments/workshop/manifests/site.pp
@@ -0,0 +1,6 @@
+# Puppet Site Manifest
+# Entry point for the workshop environment
+# Declares which classes/roles apply to nodes
+
+# Apply the workshop class to all nodes (for now)
+include workshop
